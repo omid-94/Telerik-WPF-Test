f13d6456fcfae9d24708ccb0925118dad021f1f6
omid-94 <omiddadvar94@gmail.com>
2022-12-28 18:34:35 +0330

    initial commit :grin:

---
+++ TelerikWpfApp1/Data/CarouselImageData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using TelerikWpfApp1.UserControls;
+
+namespace TelerikWpfApp1.Data
+{
+    public class CarouselImageData
+    {
+        public static List<CarouselItem> GetData()
+        {
+            var imageList = new List<CarouselItem>();
+
+            _FillData(ref imageList);
+
+            return imageList;
+        }
+        private static void _FillData(ref List<CarouselItem> list)
+        {
+            var myItem = new CarouselItem();
+            myItem.ImageSource = new BitmapImage(new Uri("../Resources/Images/scenery 01.jpg", UriKind.Relative));
+            list.Add(myItem);
+
+            var myItem1 = new CarouselItem();
+            myItem1.ImageSource = new BitmapImage(new Uri("/Resources/Images/scenery 02.jpg", UriKind.Relative));
+            list.Add(myItem1);
+
+            var myItem2 = new CarouselItem();
+            myItem2.ImageSource = new BitmapImage(new Uri("/Resources/Images/scenery 03.jpg", UriKind.Relative));
+            list.Add(myItem2);
+
+            var myItem3 = new CarouselItem();
+            myItem3.ImageSource = new BitmapImage(new Uri("/Resources/Images/scenery 01.jpg", UriKind.Relative));
+            list.Add(myItem3);
+
+            var myItem4 = new CarouselItem();
+            myItem4.ImageSource = new BitmapImage(new Uri("/Resources/Images/scenery 04.jpg", UriKind.Relative));
+            list.Add(myItem4);
+        }
+    }
+}
---
+++ TelerikWpfApp1/UserControls/CarouselItem.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TelerikWpfApp1.UserControls
+{
+    /// <summary>
+    /// Interaction logic for CarouselItem.xaml
+    /// </summary>
+    public partial class CarouselItem : UserControl
+    {
+        /// <summary>
+        /// Default ImageHeight is 200
+        /// </summary>
+        public double ImageHeight
+        {
+            get { return MyImage.Height; }
+            set { MyImage.Height = value; }
+        }
+
+        /// <summary>
+        /// Default ImageWidth is 200
+        /// </summary>
+        public double ImageWidth
+        {
+            get { return MyImage.Width; }
+            set { MyImage.Width = value; }
+        }
+        public ImageSource ImageSource
+        {
+            get { return MyImage.Source; }
+            set { MyImage.Source = value; }
+        }
+        public string HeaderText
+        {
+            get { return MyHeader.Text; }
+            set { MyHeader.Text = value; }
+        }
+        public string FooterText
+        {
+            get { return MyFooter.Text; }
+            set { MyFooter.Text = value; }
+        }
+        public CarouselItem()
+        {
+            InitializeComponent();
+
+            _InitialValues();
+        }
+        private void _InitialValues()
+        {
+            this.ImageHeight = 250;
+            this.ImageWidth = 500;
+        }
+    }
+}
